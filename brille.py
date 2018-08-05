392093f1cbdaf176e38fa6233bb9e3cdd706777f
urbanware-org <github@urbanware.org>
2018-08-05 18:24:07 +0200

    Add link to GitLab

--- brille.py
+++ brille.py
@@ -7,6 +7,7 @@
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
 # GitHub: https://github.com/urbanware-org/brille
+# GitLab: https://gitlab.com/urbanware-org/brille
 # ============================================================================
 
 __version__ = "1.0.1"
