d8187581f3370e34de4584bd73f3a8957b157096
urbanware-org <github@urbanware.org>
2023-10-26 18:21:49 +0200

    Add binary Braille alphabet

--- julia/brille.jl
+++ julia/brille.jl
@@ -12,16 +12,39 @@
 
 __version__ = "1.0.0"
 
-BRAILLE_ALPHABET = Dict([
-                    ("a", "⠁"), ("b", "⠃"), ("c", "⠉"), ("d", "⠙"),
-                    ("e", "⠑"), ("f", "⠋"), ("g", "⠛"), ("h", "⠓"),
-                    ("i", "⠊"), ("j", "⠚"), ("k", "⠅"), ("l", "⠇"),
-                    ("m", "⠍"), ("n", "⠝"), ("o", "⠕"), ("p", "⠏"),
-                    ("q", "⠟"), ("r", "⠗"), ("s", "⠎"), ("t", "⠞"),
-                    ("u", "⠥"), ("v", "⠧"), ("w", "⠺"), ("x", "⠭"),
-                    ("y", "⠽"), ("z", "⠵"), ("1", "⠼⠁"), ("2", "⠼⠃"),
-                    ("3", "⠼⠉"), ("4", "⠼⠙"), ("5", "⠼⠑"), ("6", "⠼⠋"),
-                    ("7", "⠼⠛"), ("8", "⠼⠓"), ("9", "⠼⠊"), ("0", "⠼⠚")])
+BRAILLE_ALPHABET_BINARY = Dict([
+                            ("a", "100000"), ("b", "101000"),
+                            ("c", "110000"), ("d", "110100"),
+                            ("e", "100100"), ("f", "111000"),
+                            ("g", "111100"), ("h", "101100"),
+                            ("i", "011000"), ("j", "011100"),
+                            ("k", "100010"), ("l", "101010"),
+                            ("m", "110010"), ("n", "110110"),
+                            ("o", "100110"), ("p", "111010"),
+                            ("q", "111110"), ("r", "101110"),
+                            ("s", "011010"), ("t", "011110"),
+                            ("u", "100011"), ("v", "101011"),
+                            ("w", "011101"), ("x", "110011"),
+                            ("y", "110111"), ("z", "100111"),
+                            ("0", "001111"), ("1", "001100"),
+                            ("2", "001110"), ("3", "001000"),
+                            ("4", "001010"), ("5", "001001"),
+                            ("6", "001101"), ("7", "001011"),
+                            ("8", "000101"), ("9", "000111"),
+                            (" ", "000000")])
+
+BRAILLE_ALPHABET_CHARS = Dict([
+                            ("a", "⠁"), ("b", "⠃"), ("c", "⠉"), ("d", "⠙"),
+                            ("e", "⠑"), ("f", "⠋"), ("g", "⠛"), ("h", "⠓"),
+                            ("i", "⠊"), ("j", "⠚"), ("k", "⠅"), ("l", "⠇"),
+                            ("m", "⠍"), ("n", "⠝"), ("o", "⠕"), ("p", "⠏"),
+                            ("q", "⠟"), ("r", "⠗"), ("s", "⠎"), ("t", "⠞"),
+                            ("u", "⠥"), ("v", "⠧"), ("w", "⠺"), ("x", "⠭"),
+                            ("y", "⠽"), ("z", "⠵"), ("0", "⠼⠚"),
+                            ("1", "⠼⠁"), ("2", "⠼⠃"), ("3", "⠼⠉"),
+                            ("4", "⠼⠙"), ("5", "⠼⠑"), ("6", "⠼⠋"),
+                            ("7", "⠼⠛"), ("8", "⠼⠓"), ("9", "⠼⠊"),
+                            (" ", " ")])
 
 
 function get_version()
@@ -32,10 +55,15 @@ function get_version()
 end
 
 
-function string2braille(string)
+function string2braille(string, use_binary)
     """
         Convert (encode) a string into the Braille alphabet.
     """
+    if use_binary
+        braille_alphabet = BRAILLE_ALPHABET_BINARY
+    else
+        braille_alphabet = BRAILLE_ALPHABET_CHARS
+    end
     output = ""
 
     while occursin("  ", string)
@@ -44,10 +72,18 @@ function string2braille(string)
 
     for char in lowercase(strip(string))
         try
-            char = BRAILLE_ALPHABET["$char"]
-            output = "$output$char"
+            char = braille_alphabet["$char"]
+            if use_binary
+                output = "$output $char"
+            else
+                output = "$output$char"
+            end
         catch
-            output = "$output$char"
+            if use_binary
+                output = "$output $char"
+            else
+                output = "$output$char"
+            end
         end
     end
 
@@ -55,23 +91,41 @@ function string2braille(string)
 end
 
 
-function braille2string(braille_code)
+function braille2string(braille_code, use_binary)
     """
         Convert (decode) the Braille alphabet into a string.
     """
+    if use_binary
+        braille_alphabet = BRAILLE_ALPHABET_BINARY
+    else
+        braille_alphabet = BRAILLE_ALPHABET_CHARS
+    end
     output = ""
 
     while occursin("  ", braille_code)
         braille_code = replace(braille_code, "  " => " ")
     end
 
-    braille = Dict(value => key for (key, value) in BRAILLE_ALPHABET)
-    for char in braille_code
-        try
-            char = braille["$char"]
-            output = "$output$char"
-        catch
-            output = "$output$char"
+    if use_binary
+        braille = Dict(v => k for (k, v) in braille_alphabet)
+        output = ""
+        for char in split(braille_code, " ")
+            braille_char = get(braille, char, "")
+            if braille_char == ""
+                output = "$output$char"
+            else
+                output = "$output$braille_char"
+            end
+        end
+    else
+        braille = Dict(v => k for (k, v) in braille_alphabet)
+        for char in braille_code
+            try
+                char = braille["$char"]
+                output = "$output$char"
+            catch
+                output = "$output$char"
+            end
         end
     end
 
--- python/brille.py
+++ python/brille.py
@@ -12,15 +12,30 @@
 
 __version__ = "1.0.2"
 
-BRAILLE_ALPHABET = {"a": "⠁", "b": "⠃", "c": "⠉", "d": "⠙",
-                    "e": "⠑", "f": "⠋", "g": "⠛", "h": "⠓",
-                    "i": "⠊", "j": "⠚", "k": "⠅", "l": "⠇",
-                    "m": "⠍", "n": "⠝", "o": "⠕", "p": "⠏",
-                    "q": "⠟", "r": "⠗", "s": "⠎", "t": "⠞",
-                    "u": "⠥", "v": "⠧", "w": "⠺", "x": "⠭",
-                    "y": "⠽", "z": "⠵", "1": "⠼⠁", "2": "⠼⠃",
-                    "3": "⠼⠉", "4": "⠼⠙", "5": "⠼⠑", "6": "⠼⠋",
-                    "7": "⠼⠛", "8": "⠼⠓", "9": "⠼⠊", "0": "⠼⠚"}
+BRAILLE_ALPHABET_BINARY = {"a": "100000", "b": "101000", "c": "110000",
+                           "d": "110100", "e": "100100", "f": "111000",
+                           "g": "111100", "h": "101100", "i": "011000",
+                           "j": "011100", "k": "100010", "l": "101010",
+                           "m": "110010", "n": "110110", "o": "100110",
+                           "p": "111010", "q": "111110", "r": "101110",
+                           "s": "011010", "t": "011110", "u": "100011",
+                           "v": "101011", "w": "011101", "x": "110011",
+                           "y": "110111", "z": "100111", "0": "001111",
+                           "1": "001100", "2": "001110", "3": "001000",
+                           "4": "001010", "5": "001001", "6": "001101",
+                           "7": "001011", "8": "000101", "9": "000111",
+                           " ": "000000"}
+
+BRAILLE_ALPHABET_CHARS = {"a": "⠁", "b": "⠃", "c": "⠉", "d": "⠙",
+                          "e": "⠑", "f": "⠋", "g": "⠛", "h": "⠓",
+                          "i": "⠊", "j": "⠚", "k": "⠅", "l": "⠇",
+                          "m": "⠍", "n": "⠝", "o": "⠕", "p": "⠏",
+                          "q": "⠟", "r": "⠗", "s": "⠎", "t": "⠞",
+                          "u": "⠥", "v": "⠧", "w": "⠺", "x": "⠭",
+                          "y": "⠽", "z": "⠵", "1": "⠼⠁", "2": "⠼⠃",
+                          "3": "⠼⠉", "4": "⠼⠙", "5": "⠼⠑", "6": "⠼⠋",
+                          "7": "⠼⠛", "8": "⠼⠓", "9": "⠼⠊", "0": "⠼⠚",
+                          " ": " "}
 
 
 def get_version():
@@ -30,38 +45,57 @@ def get_version():
     return __version__
 
 
-def string2braille(string):
+def string2braille(string, use_binary=False):
     """
         Convert (encode) a string into the Braille alphabet.
     """
+    if use_binary:
+        braille_alphabet = BRAILLE_ALPHABET_BINARY
+    else:
+        braille_alphabet = BRAILLE_ALPHABET_CHARS
     output = ""
 
     while " " * 2 in string:
         string = string.replace((" " * 2), " ")
 
     for char in string.lower().strip():
-        braille_char = BRAILLE_ALPHABET.get(char, "")
+        braille_char = braille_alphabet.get(char, "")
         if braille_char == "":
             output += char
         else:
             output += braille_char
+            if use_binary:
+                output += " "
 
     return output.strip()
 
 
-def braille2string(braille_code):
+def braille2string(braille_code, use_binary=False):
     """
         Convert (decode) the Braille alphabet into a string.
     """
+    if use_binary:
+        braille_alphabet = BRAILLE_ALPHABET_BINARY
+    else:
+        braille_alphabet = BRAILLE_ALPHABET_CHARS
     output = ""
 
-    braille = {v: k for k, v in BRAILLE_ALPHABET.items()}
-    for char in braille_code:
-        braille_char = braille.get(char, "")
-        if braille_char == "":
-            output += char
-        else:
-            output += braille_char
+    if use_binary:
+        braille = {v: k for k, v in braille_alphabet.items()}
+        for char in braille_code.split(" "):
+            braille_char = braille.get(char, "")
+            if braille_char == "":
+                output += char
+            else:
+                output += braille_char
+    else:
+        braille = {v: k for k, v in braille_alphabet.items()}
+        for char in braille_code:
+            braille_char = braille.get(char, "")
+            if braille_char == " ":
+                output += char
+            else:
+                output += braille_char
 
     return output.strip()
 
